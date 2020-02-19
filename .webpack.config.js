3f506a9a1edff8487e20f44c98545ebc472b951c
Tom <emailfortomg@gmail.com>
2020-02-19 19:05:27 +0000

    Better Webpack Output Filename for browser engine

--- .webpack.config.js
+++ .webpack.config.js
@@ -5,12 +5,12 @@ const package = require("./package.json");
 module.exports = {
   entry: {
     StormDB: './src/stormdb.js',
-    browserEngine: './src/engine/browser.js'
+    BrowserEngine: './src/engine/browser.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].min.js',
     library: '[name]'
   },
-  plugins: [new Webpack.BannerPlugin(`StormDB ${ package.version }`)],
+  plugins: [new Webpack.BannerPlugin(`StormDB ${ package.version }`)]
 };
