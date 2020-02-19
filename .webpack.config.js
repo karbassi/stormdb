546586950f7125d117534e8995d6576229bc9daf
Tom <emailfortomg@gmail.com>
2020-02-19 19:37:57 +0000

    Formatted .webpack.config.js

--- .webpack.config.js
+++ .webpack.config.js
@@ -1,16 +1,16 @@
 const Webpack = require("webpack");
-const path = require('path');
+const path = require("path");
 const package = require("./package.json");
 
 module.exports = {
   entry: {
-    StormDB: './src/stormdb.js',
-    BrowserEngine: './src/engine/browser.js'
+    StormDB: "./src/stormdb.js",
+    BrowserEngine: "./src/engine/browser.js"
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].min.js',
-    library: '[name]'
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].min.js",
+    library: "[name]"
   },
-  plugins: [new Webpack.BannerPlugin(`StormDB ${ package.version }`)]
+  plugins: [new Webpack.BannerPlugin(`StormDB ${package.version}`)]
 };
